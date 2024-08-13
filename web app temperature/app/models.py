cbf2052255883a47c9b637fc9c814d13d04435df
muswabadavid <129625732+muswabadavid@users.noreply.github.com>
2024-08-13 12:07:43 +0200

    Delete web app temperature directory

--- web app temperature/app/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Temperature(models.Model):
-    data = models.CharField(max_length=20)
--- web app temperature/app/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Temperature
-
-class TemperatureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Temperature
-        fields = ['data']
